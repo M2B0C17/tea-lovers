258dc10e8ce83ca5fcb74de81eb453e6753382f7
macarena baltra <macarena.bc2011@gmail.com>
2017-05-18 23:10:11 -0400

    Subiendo parte 2 - Evaluado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,34 @@
 //Validar Datos
 function validar(){
   // "cajas" donde almacenaremos los datos de los input (id)
-  var nombre = "";
-  nombre.document.getElementById('nombre').value;
-  var email = "";
-  nombre.document.getElementById('email').value;
-  var direccion = "";
-  nombre.document.getElementById('direc').value;
+  var nombre = document.getElementById('nombre').value = "";
+  var email = document.getElementById('email').value = "";
+  var direccion = document.getElementById('direc').value = "";
 
   // evaluando los campos
+  /*
   var expressionEmail = /\w+@\w+\.+[a-z]/;
   var expressionNombre = /[a-z]/;
+  */
+    /*
+    else if (email != expressionEmail){
+      alert("Su email no es valido");
+    }else if (nombre != expressionNombre){
+      alert("Su nombre no es valido");
+    }
+    */
 
   // boton
   var boton = document.getElementById('suscribirse');
   boton.addEventListener('click', function(){
 
-    if(nombre === "" || email === "" || direccion === ""){
+    if(nombre === ""){
       alert("Todos los campos son obligatorios");
-    }else if (email != expressionEmail){
-      alert("Su email no es valido");
-    }else if (nombre != expressionNombre){
-      alert("Su nombre no es valido");
-    }else {
+    }else{
       alert("Muchas gracias por suscribirse");
     }
-  });
-}
+  };)
+  }
+
+  // Seleccion de Té
+  var seleccionUsuario = document.getElementById("selec").value;
